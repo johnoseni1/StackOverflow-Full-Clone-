42024f58a96de675f2531d969b7857852a18adec
John Oseni <johnoseni26@gmail.com>
2022-02-03 12:52:03 +0100

    fixed the bug in middleware of backend

--- api/middlewares/answerAuth.js
+++ api/middlewares/answerAuth.js
@@ -1,3 +1,5 @@
+
+
 const answerAuth = (req, res, next) => {
   if (req.answer.author._id.equals(req.user.id) || req.user.role === 'admin') return next();
   res.status(401).end();
--- api/middlewares/questionAuth.js
+++ api/middlewares/questionAuth.js
@@ -1,3 +1,4 @@
+
 const questionsAuth = (req, res, next) => {
   if (req.question.author._id.equals(req.user.id) || req.user.role === 'admin') return next();
   res.status(401).end();
